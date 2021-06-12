734c693bd639157e26a6338a57c3270ce3409c89
Ye Chuah <yechuah@gmail.com>
2021-06-12 18:53:50 +1000

    Empty page (#5)
    * firebase
    
    * redirect to invoice from empty page

--- src/views/Empty/index.tsx
+++ src/views/Empty/index.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { v4 } from "uuid";
+import { useInvoices } from "../../useInvoice";
 
 export function Empty() {
   const navigate = useNavigate();
-  return (
-    <div>
-      <button onClick={() => navigate(`/invoicer/invoice/${v4()}`)}>
-        New Invoice
-      </button>
-    </div>
-  );
+  const invoices = useInvoices();
+
+  React.useEffect(() => {
+    if (invoices.loaded) {
+      if (invoices.items.length > 0) {
+        navigate(`/invoicer/invoice/${invoices.items[0]._id}`);
+      } else {
+        navigate(`/invoicer/invoice/${v4()}`);
+      }
+    }
+  }, [invoices.loaded]);
+  return null;
 }
